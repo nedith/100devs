c953d97255bf0e6ea44e0da2d208914dccba22af
nedith <nedith22@gmail.com>
2022-10-26 14:50:19 +0300

    Another way to creat object

--- class22-materials/objects-lost-galaxy/js/main.js
+++ class22-materials/objects-lost-galaxy/js/main.js
@@ -1,10 +1,20 @@
 //Create a mouse object that has four properties and three methods
+// Solution 1
 const mouse = {
- color: 'grey',
- size: 'large',
- name: 'squeeke',
- speak() {
- return `Iam ${this.name} and I am ${this.size} and ${this.color}`
- }
-}
+  color: 'grey',
+  size: 'large',
+  name: 'squeeke',
+  speak() {
+    return `Iam ${this.name} and I am ${this.size} and ${this.color}`;
+  },
+};
 console.log(mouse.speak());
+
+// Solution 2
+const rat = {};
+rat.color = 'white';
+(rat.size = 'small'), (rat.name = 'ratou');
+rat.speak = function () {
+  return `Iam ${this.name} and I am ${this.size} and ${this.color}`;
+};
+console.log(rat.speak())
