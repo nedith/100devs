3372621faa883ba92a0c1b8849c2717bd0a3cbf3
nedith <nedith22@gmail.com>
2022-10-27 16:30:40 +0300

    Create constructor

--- class22-materials/objects-street-fighter/js/main.js
+++ class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,18 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+function StreetFighter(name, strength, move, lives) {
+  this.name = name;
+  this.strength = strength;
+  this.move = move;
+  this.lives = lives;
+  this.specialMove = function () {
+    alert('KICK!!!');
+  };
+  this.profile = function () {
+    alert(`${this.name} and my move is ${this.move} and I have ${lives} lives`);
+  };
+  this.superPower = function () {
+    alert(`My strength is ${strength}`);
+  };
+}
+
+const sudo = new StreetFighter('sudo', 'cute', 'kick', 9);
