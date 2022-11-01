e2ed392b59d2c23c0112bfd28a9cf2b2bd099687
nedith <nedith22@gmail.com>
2022-11-01 13:44:17 +0300

    Add solution

---
+++ space-review/js/main.js
@@ -0,0 +1,18 @@
+//Arrays
+
+//Create an array of numbers. Sum all of the numbers. Alert the sum.
+let numbers = [2, 4, 5, 8, 10, 14, 20];
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+alert(sum);
+
+//Create a function that takes in an array of numbers
+//Return a new array of numbers that is every original number squared
+
+//Create a function that takes string
+//Print the reverse of that string to the console
+
+//Create a function that takes in a string
+//Alert if the string is a palindrome or not
