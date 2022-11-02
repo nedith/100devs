2136524a4d9f5973e8f54177342882b8e3e0c62a
nedith <nedith22@gmail.com>
2022-11-02 13:47:53 +0300

    Add optimized solution

--- space-review/js/main.js
+++ space-review/js/main.js
@@ -2,11 +2,15 @@
 
 //Create an array of numbers. Sum all of the numbers. Alert the sum.
 let numbers = [2, 4, 5, 8, 10, 14, 20];
-let sum = 0;
-for (let i = 0; i < numbers.length; i++) {
-  sum += numbers[i];
-}
-alert(sum);
+// Solution 1
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+// alert(sum);
+
+// Solution 2
+alert(numbers.reduce((sum, c) => sum + c, 0));
 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
