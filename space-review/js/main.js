3367909de677569a24cd93aa820e011d4d6aad3a
nedith <nedith22@gmail.com>
2022-11-04 14:57:07 +0300

    Reverse string

--- space-review/js/main.js
+++ space-review/js/main.js
@@ -22,12 +22,16 @@ function arrNumbers(nums) {
   // return newArray;
 
   // Solution 2
-  return nums.map((num) => (num * num));
+  return nums.map((num) => num * num);
 }
 console.log(arrNumbers([1, 2, 3, 4, 5]));
 
 //Create a function that takes string
 //Print the reverse of that string to the console
+const reverseString = (str) => {
+  console.log(str.split('').reverse().join(''));
+};
+reverseString('edith');
 
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
