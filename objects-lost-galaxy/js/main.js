42865ac0c31e2cf7877e7941801c786569ff3acd
nedith <nedith22@gmail.com>
2022-11-09 16:48:53 +0300

    Create object using template literal

--- objects-lost-galaxy/js/main.js
+++ objects-lost-galaxy/js/main.js
@@ -1,23 +1,46 @@
 //Create a dog object that has four properties and three methods
-function Dog(name, breed, color, owner) {
-  this.name = name;
-  this.breed = breed;
-  this.color = color;
-  this.owner = owner;
-  this.speak = function () {
-    console.log('woof woof');
-  };
-  this.profile = function () {
-    console.log(
-      `I am called ${this.name}, a ${this.color} ${this.breed} dog onwed by ${this.owner}`
-    );
-  };
-  this.favTreat = function () {
-    console.log('I love buscuits!!!');
-  };
-}
+// Constructor method
+// function Dog(name, breed, color, owner) {
+//   this.name = name;
+//   this.breed = breed;
+//   this.color = color;
+//   this.owner = owner;
+//   this.speak = function () {
+//     console.log('woof woof');
+//   };
+//   this.profile = function () {
+//     console.log(
+//       `I am called ${this.name}, a ${this.color} ${this.breed} dog owned by ${this.owner}`
+//     );
+//   };
+//   this.favTreat = function () {
+//     console.log('I love buscuits!!!');
+//   };
+// }
 
-const fido = new Dog('fido', 'labrador', 'black', 'edith');
-fido.speak();
-fido.profile();
-fido.favTreat();
+// const fido = new Dog('fido', 'labrador', 'black', 'edith');
+// fido.speak();
+// fido.profile();
+// fido.favTreat();
+
+// Template literal
+const dog = {
+  name: 'terry',
+  breed: 'fox',
+  color: 'white',
+  owner: 'henry',
+  speak: function () {
+    console.log('woof woof');
+  },
+  profile: function () {
+    console.log(
+      `I am called ${this.name}, a ${this.color} ${this.breed} dog owned by ${this.owner}`
+    );
+  },
+  favTreat: function () {
+    console.log('I love buscuits!!!');
+  },
+};
+dog.speak();
+dog.profile();
+dog.favTreat();
