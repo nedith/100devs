10537ecb5735fa6824f5bdccd227bc0f8bdf4439
nedith <nedith22@gmail.com>
2022-11-11 16:22:11 +0300

    Use NASA api

--- api-template/js/main.js
+++ api-template/js/main.js
@@ -1,17 +1,18 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('button').addEventListener('click', getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value.toLowerCase()
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function getFetch() {
+  const choice = document.querySelector('input').value.toLowerCase();
+  const url = `https://api.nasa.gov/planetary/apod?api_key=PDwymXH9mdhX1AGuqqQpwPT2GwcbClSPfy9KoBnb&date=${choice}`;
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      document.querySelector('img').src = data.hdurl;
+      document.querySelector('h3').innerText += data.explanation;
+        })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
-
