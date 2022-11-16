9cd686b3278543841c4c735ce38c81c1edc4065b
nedith <nedith22@gmail.com>
2022-11-16 12:12:38 +0300

    Put name of spell into the DOM

--- dnd/js/main.js
+++ dnd/js/main.js
@@ -1,17 +1,18 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('button').addEventListener('click', getFetch);
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+function getFetch() {
+  const choice = document.querySelector('input').value;
+  const url = `https://www.dnd5eapi.co/api/spells/${choice}`;
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-       
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      document.querySelector('h2').innerText += data.name;
+      document.querySelector('h3').innerText += data.classes[0].name;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
-
