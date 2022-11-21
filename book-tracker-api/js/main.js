872d3ff206254334f5ce9cb7e6b23beab0277358
nedith <nedith22@gmail.com>
2022-11-21 16:46:24 +0300

    Add title to local storage

--- book-tracker-api/js/main.js
+++ book-tracker-api/js/main.js
@@ -1,6 +1,9 @@
 //Example fetch using openlibrary.org
 document.querySelector('button').addEventListener('click', getTitle);
 
+// Book to persist in the DOM on page load
+document.querySelector('h2').innerText = localStorage.getItem('books') + ',';
+
 function getTitle() {
   const choice = document.querySelector('input').value;
   const url = `https://openlibrary.org/isbn/${choice}.json`;
@@ -10,7 +13,10 @@ function getTitle() {
     .then((data) => {
       console.log(data.title);
 
-      document.querySelector('h2').innerText += data.title;
+      // Store title in local storage
+      localStorage.setItem('books', data.title);
+      document.querySelector('h2').innerText =
+        localStorage.getItem('books') + '.';
     })
     .catch((err) => {
       console.log(`error ${err}`);
