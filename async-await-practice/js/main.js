1f85c6be388c159764c207371f9a7483ee94fcc0
nedith <nedith22@gmail.com>
2023-02-27 13:52:19 +0300

    Add async function

--- async-await-practice/js/main.js
+++ async-await-practice/js/main.js
@@ -1,20 +1,17 @@
-// Using Promise.all() method
-const fetchPromise1 = fetch(
-  'https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json'
-);
-const fetchPromise2 = fetch(
-  'https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found'
-);
-const fetchPromise3 = fetch(
-  'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json'
-);
-
-Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
-  .then((responses) => {
-    for (const response of responses) {
-      console.log(`${response.url}: ${response.status}`);
-    }
-  })
-  .catch((error) => {
+// Using async/await syntax
+async function fetchProducts() {
+  try {
+    const response = await fetch(
+      'https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json'
+    );
+    console.log(response);
+    if(!response.ok)
+    throw new Error(`HTTP Error: ${response.status}`)
+    const data = await response.json()
+    console.log(data[0].name)
+  } catch (error) {
     console.log(`Failed to fetch: ${error}`);
-  });
+  }
+}
+
+fetchProducts();
