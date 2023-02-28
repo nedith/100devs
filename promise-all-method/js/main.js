2df37ef238f8dbc816f6b9116468ad9cde13deb0
nedith <nedith22@gmail.com>
2023-02-28 12:05:31 +0300

    Add main.js file

---
+++ promise-all-method/js/main.js
@@ -0,0 +1,21 @@
+// Getting data from can-store API using fetch() API
+const fetchedPromise = fetch(
+  'https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json'
+);
+
+console.log(fetchedPromise);
+
+fetchedPromise
+  .then((response) => {
+    if (!response.ok) throw new Error(`HTTP Error: ${response.status}`);
+  else return response.json();
+  
+  })
+  .then((data) => {
+    console.log(data[0].name);
+  })
+  .catch((error) => {
+    console.log(`Could not get products: ${error}`);
+  });
+
+console.log('Started Request...');
