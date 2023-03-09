c3dfaf94c5b8df716713a50ef04c57796ec4856f
nedith <nedith22@gmail.com>
2023-03-09 12:29:36 +0300

    Create Animal class

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,9 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
-
+class Animal {
+ constructor(name) {
+  this.name = name;
+ }
+ speack() {
+  console.log(`${this.name} is speaking!!`);
+ }
+}
