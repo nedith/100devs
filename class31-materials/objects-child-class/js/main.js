d8f07389902367568b7b18d7d80c00b5ae623d17
nedith <nedith22@gmail.com>
2023-03-17 12:01:20 +0300

    Add index.html file

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,16 +1,18 @@
-//Create an a class and extend it - Can be anything you would like it to be! 
+//Create an a class and extend it - Can be anything you would like it to be!
 class Animal {
- constructor(name) {
-  this.name = name;
- }
- speack() {
-  console.log(`${this.name} is speaking!!`);
- }
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    console.log(`${this.name} is speaking!!`);
+  }
 }
 
-class Dog extends animal {
- constructor(name, breed) {
-  super(name);
-  this.breed = breed;
- }
+class Dog extends Animal {
+  constructor(name, breed) {
+    super(name);
+    this.breed = breed;
+  }
 }
+
+const simba = new Dog('Simba', 'Sheperd');
