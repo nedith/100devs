760f047379a9362fb4b8a3832cc549feed275006
nedith <nedith22@gmail.com>
2023-03-23 14:04:32 +0300

    File system practice

---
+++ node-practice/reference/fs-demo.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+// Create folder
+// fs.mkdir(path.join(__dirname, '/test'), (err) => {
+//   if (err) throw new Error(err);
+//   console.log('Folder created...');
+// });
+
+// Create and write to file
+// fs.writeFile(
+//   path.join(__dirname, '/test', 'hello.txt'),
+//   'Hello World!',
+//   (err) => {
+//     if (err) throw err;
+//     console.log('File written into...');
+
+//     fs.appendFile(
+//       path.join(__dirname, '/test', 'hello.txt'),
+//       'I love Node!',
+//       (err) => {
+//         if (err) throw err;
+//         console.log('File written into...');
+//       }
+//     );
+
+//   }
+
+// );
+
+// Read file
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8',(err,data) => {
+//   if (err) throw new Error(err);
+//   console.log(data);
+// });
+
+// Rename file
+fs.rename(
+  path.join(
+    __dirname,
+    '/test',
+    'hello.txt'),
+    path.join(__dirname, '/test', 'world.txt'),
+ 
+  (err) => {
+    if (err) throw new Error(err);
+    console.log('File renamed...');
+  }
+);
