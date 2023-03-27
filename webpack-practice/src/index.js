c3d394cda4527a81abb108db2280b74cd36257a0
nedith <nedith22@gmail.com>
2023-03-27 13:06:03 +0300

    Set up and configure webpack

--- webpack-practice/src/index.js
+++ webpack-practice/src/index.js
@@ -1,5 +1,5 @@
 const moment = require('moment');
 
-console.log('Hello from JavaScript!');
+console.log('Hello JavaScript!');
 console.log(moment().startOf('day').fromNow());
 console.log(moment().endOf('day').fromNow());
